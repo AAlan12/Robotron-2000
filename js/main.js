f887a9dcd886bfc444acfd7ccfe7d2feb9c87466
Antonio Alan <tonio.alan07@gmail.com>
2023-01-25 10:15:58 -0300

    init de integração de logica para estatisticas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
 const controle = document.querySelectorAll("[data-controle]");
 
+const pecas = {
+    "bracos": {
+        "força": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+    "blindagem": {
+        "força": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos": {
+        "força": 0,
+        "poder": 7,
+        "energia": 40,
+        "velocidade": -4
+    },
+    "pernas": {
+        "força": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 43
+    },
+    "foguetes": {
+        "força": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    },
+}
+
 controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
         manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
